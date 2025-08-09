a91e7afa67b572a048468a129e154b2b55c723b0
minjeong46 <eyou46@naver.com>
2025-08-10 00:12:00 +0900

    feat. categoryFilter, srotFilter 구현

---
+++ src/componentes/AccountBookSort.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const AccountBookSort = ({
+    categoryList,
+    getSelectedCategoryFilter,
+    getSelectedSortFilter,
+}) => {
+    const sortFilter = [
+        { label: "가격 높은 순", value: "priceHigh" },
+        { label: "가격 낮은 순", value: "priceLow" },
+        { label: "최신 순", value: "latest" },
+        { label: "오래된 순", value: "earliest" },
+    ];
+
+    const onChangeCategory = (e) => {
+        getSelectedCategoryFilter(e.target.value);
+    };
+
+    const onChangeSortFilter = (e) => {
+        getSelectedSortFilter(e.target.value);
+    };
+
+    return (
+        <div>
+            <select onChange={onChangeCategory}>
+                <option value="">유형 필터</option>
+                {categoryList.map((category) => {
+                    return (
+                        <option key={category.value} value={category.value}>
+                            {category.label}
+                        </option>
+                    );
+                })}
+            </select>
+            <select onChange={onChangeSortFilter}>
+                <option value="">정렬 기준</option>
+                {sortFilter.map((sort) => {
+                    return (
+                        <option key={sort.value} value={sort.value}>
+                            {sort.label}
+                        </option>
+                    );
+                })}
+            </select>
+            <div>
+                <input type="date" placeholder="시작"></input>
+                <input type="date" placeholder="종료"></input>
+            </div>
+        </div>
+    );
+};
+
+export default AccountBookSort;
